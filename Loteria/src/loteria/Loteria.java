1c128551c2928511b306a7fb186aa9d036ac7304
Marcos Pilger <marcosapilger@gmail.com>
2019-08-29 22:21:00 -0300

    Criada array para pegar 6 numeros

--- Loteria/src/loteria/Loteria.java
+++ Loteria/src/loteria/Loteria.java
@@ -5,15 +5,42 @@ import java.util.Scanner;
 public class Loteria {
 
     public static void main(String[] args) {
+        int[] valores = new int[] {0,0,0,0,0,0};
+        imprimeVetor(valores);
+//        int[] valores2;
+//        valores2 = new int[6];
+//        valores2[0] = 0;
+//        valores2[1] = 0;
+//        valores2[2] = 0;
+//        valores2[3] = 0;
+//        valores2[4] = 0;
+//        valores2[5] = 0;
+        
+        
         Scanner in = new Scanner(System.in);
         System.out.println("Digite 6 Numeros.");
         for (int i = 0; i < 6; i++) {
-            capNum(in, i);
+            int num = capNum(in, i);
+            while (num == -1) {
+                num = capNum(in, i);
+            }
         }
+
     }
 
     public static int capNum(Scanner in, int i) {
         System.out.println("Digite o " + (i + 1) + "º numero");
-        return 0;
+        int num = in.nextInt();
+        if (num >= 1 && num <= 60) {
+            return num;
+        } else {
+            return -1;
+        }
     }
+    public static void imprimeVetor(int[]vet){
+        for (int i = 0; i < vet.length; i++) {
+            System.out.print(vet[i]+" ");
+        }
+        System.out.println(" ");
+}
 }
