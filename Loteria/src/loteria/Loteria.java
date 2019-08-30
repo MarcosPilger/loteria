717fe6e852a58059a71f6bef9809f77dec102f2a
Marcos Pilger <marcosapilger@gmail.com>
2019-08-29 22:40:22 -0300

    validar numeros repitidos

--- Loteria/src/loteria/Loteria.java
+++ Loteria/src/loteria/Loteria.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Loteria {
 
     public static void main(String[] args) {
-        int[] valores = new int[] {0,0,0,0,0,0};
+        int[] valores = new int[]{0, 0, 0, 0, 0, 0};
         imprimeVetor(valores);
 //        int[] valores2;
 //        valores2 = new int[6];
@@ -15,14 +15,20 @@ public class Loteria {
 //        valores2[3] = 0;
 //        valores2[4] = 0;
 //        valores2[5] = 0;
-        
-        
+
         Scanner in = new Scanner(System.in);
         System.out.println("Digite 6 Numeros.");
         for (int i = 0; i < 6; i++) {
-            int num = capNum(in, i);
+            int num = -1;
             while (num == -1) {
                 num = capNum(in, i);
+                for (int j = 0; j < i; j++) {
+                    if (valores[j] == num) {
+                        num = -1;
+                        break;
+                    }
+
+                }
             }
             valores[i] = num;
             imprimeVetor(valores);
@@ -39,10 +45,11 @@ public class Loteria {
             return -1;
         }
     }
-    public static void imprimeVetor(int[]vet){
+
+    public static void imprimeVetor(int[] vet) {
         for (int i = 0; i < vet.length; i++) {
-            System.out.print(vet[i]+" ");
+            System.out.print(vet[i] + " ");
         }
         System.out.println(" ");
-}
+    }
 }
